e09742b056670ee66c13ac0a28ac191006eda7df
taka.oyama <taka.oyama@gmail.com>
2016-06-07 01:41:21 +0900

    move folder

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var socket = require('./lib/socket');
 var config = require('./lib/config');
-var server = require('./app/express').init();
+var server = require('./lib/server').init();
 var bounce = socket.init(server, config);
-
